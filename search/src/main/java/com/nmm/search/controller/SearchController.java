b72c344c98bc19a166d68f460fd5295886b6af02
01479820 <niemingming@haier.com>
2018-03-20 17:35:14 +0800

    界面问题调试

--- search/src/main/java/com/nmm/search/controller/SearchController.java
+++ search/src/main/java/com/nmm/search/controller/SearchController.java
@@ -3,14 +3,15 @@ package com.nmm.search.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SearchController {
 
     @ResponseBody
-    @RequestMapping("/searchData/{name}")
-    public String searchData(@PathVariable String name){
+    @RequestMapping("/searchData")
+    public String searchData(@RequestParam(name = "username") String name){
         return name + System.currentTimeMillis();
     }
 
